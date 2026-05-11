84cdbaa766c06154be3ff355dd4e36372d17d184
JRuizGil <joseramon_ruiz@euneiz.com>
2026-05-11 18:34:23 +0200

    Pluginchange

--- Source/RedesUECooking/Private/PJR_MenuSessionFlow.cpp
+++ Source/RedesUECooking/Private/PJR_MenuSessionFlow.cpp
@@ -8,6 +8,8 @@ APJR_MenuSessionFlow::APJR_MenuSessionFlow()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	ComponenteA = CreateDefaultSubobject<UMiComponenteA>(TEXT("ComponenteA"));
+
 
 }
 
--- Source/RedesUECooking/Public/PJR_MenuSessionFlow.h
+++ Source/RedesUECooking/Public/PJR_MenuSessionFlow.h
@@ -18,6 +18,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UMiComponenteA> ComponenteA;
+
 
 public:	
 	// Called every frame
